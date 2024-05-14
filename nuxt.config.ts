066dec71396415a098db3d6b3d0fc55384ed36e6
Guayabas <guayabas0399@gmail.com>
2024-05-13 20:51:27 -0600

    redoin css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-primevue", 'nuxt-svgo'],
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
   postcss: {
     plugins: {
       tailwindcss: {},
---
+++ pages/forgotPassword.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        forgot password
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
